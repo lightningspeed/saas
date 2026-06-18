2417fa7e51a150f0dc12153eb568975c7f870d71
David Sardana <david.sardana@gmail.com>
2026-06-17 20:57:23 -0700

    Enhance project setup with environment configurations, update scripts for concurrent development, and improve homepage with AI business idea generation features. Added meta description for SEO and integrated Clerk for user authentication.

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  async rewrites() {
+    if (process.env.NODE_ENV !== "production") {
+      return [
+        {
+          source: "/api",
+          destination: "http://127.0.0.1:8000/",
+        },
+      ];
+    }
+    return [];
+  },
 };
 
 export default nextConfig;
